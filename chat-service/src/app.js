591ba23c388f57b2efbcf7feeea7e0b2aa59af47
Sagar Shirgaonkar <sagarshirgaonkar59511@gmail.com>
2024-12-06 00:13:46 +0530

    Initialized

---
+++ chat-service/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors({
+    origin:"*",
+    methods:"GET,HEAD,PUT,PATCH,POST,DELETE",
+}))
+
+app.use(express.urlencoded({extended:true,limit:"16kb"}));
+app.use(express.json({limit:"16kb"}));
+
+app.get("/",(req,res)=>{
+    res.status(200).json({"Chat-Service Status":"Connected"});
+})
+
+export {app}

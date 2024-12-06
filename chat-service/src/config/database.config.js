149971586fcb2b454369111c17a8fba43d2bce0f
Sagar Shirgaonkar <sagarshirgaonkar59511@gmail.com>
2024-12-06 19:00:15 +0530

    uri changed db

--- chat-service/src/config/database.config.js
+++ chat-service/src/config/database.config.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async()=>{
     try {
-        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`);
         console.log(`MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MongoDB Connection Failed : ",error);

6154d45e8eafb60e51e0915224a9f3600d4fb457
Sagar Shirgaonkar <sagarshirgaonkar59511@gmail.com>
2024-12-06 19:07:15 +0530

    db changed

--- chat-service/src/config/database.config.js
+++ chat-service/src/config/database.config.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async()=>{
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`);
+        const DB_NAME = "platesofhopes"
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MongoDB Connection Failed : ",error);
